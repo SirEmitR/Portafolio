b5f8693892e3fa2e6f937589fb936819fba27a70
SirEmitR <emit3reyes@gmail.com>
2024-07-23 22:52:06 -0600

    Changes

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 const modal = document.getElementById('modalOpener');
-
-
+const contactForm = document.getElementById('contact-form');
+const error = document.getElementById('error');
+const send = document.getElementById('status');
 function closeModal() {
   modal.removeAttribute('open');
 }
 
 //listen window location to close modal when click some route
 window.addEventListener('popstate', closeModal);
+
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  error.classList.add('hidden');
+  send.textContent = 'Starting';
+  fetch('https://celebra.solsoftware.mx/api/contact-portafolio', {
+    method: 'POST',
+    body: new FormData(contactForm),
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    }
+  }).then((response) => {
+    if (response.ok) {
+      closeModal();
+      contactForm.reset();
+      alert('Mensaje enviado correctamente');
+    } else {
+      error.classList.remove('hidden');
+      error.textContent = 'Error al enviar el mensaje';
+    }
+  }).finally(() => {
+    send.textContent = 'Start';
+  });
+});
