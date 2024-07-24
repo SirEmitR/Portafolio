330799be84e44ad861da416ab4cb672a78ac8cf2
SirEmitR <emit3reyes@gmail.com>
2024-07-24 01:19:35 -0600

    CVasi fin

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const modal = document.getElementById('modalOpener');
 const contactForm = document.getElementById('contact-form');
 const error = document.getElementById('error');
 const send = document.getElementById('status');
+const responseDiv = document.getElementById('response');
+
 function closeModal() {
   modal.removeAttribute('open');
 }
@@ -12,25 +14,33 @@ window.addEventListener('popstate', closeModal);
 contactForm.addEventListener('submit', (e) => {
   e.preventDefault();
   error.classList.add('hidden');
+  responseDiv.classList.add('hidden');
   send.textContent = 'Starting';
-  fetch('https://celebra.solsoftware.mx/api/contact-portafolio', {
+  const formData = new FormData(contactForm);
+  const email = formData.get('email');
+  const name = formData.get('name');
+  const message = formData.get('message') || '';
+
+  fetch('https://server-portafolio.fly.dev/v1/create', {
     method: 'POST',
-    body: new FormData(contactForm),
+    body: JSON.stringify({ email, name, message }),
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json'
     }
   }).then((response) => {
     if (response.ok) {
       closeModal();
       contactForm.reset();
-      alert('Mensaje enviado correctamente');
+      responseDiv.classList.remove('hidden');
+
+      setTimeout(() => {
+        responseDiv.classList.add('hidden');
+      }, 3000);
     } else {
       error.classList.remove('hidden');
       error.textContent = 'Error al enviar el mensaje';
     }
   }).finally(() => {
     send.textContent = 'Start';
-  });
+  }); 
 });
